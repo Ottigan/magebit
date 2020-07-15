7b08a01d5189c49fa5f57a820e31ad52852ee711
Ottigan <jmalcans91@gmail.com>
2020-07-15 12:40:11 +0300

    Added log out funcionality and limited access to Profile.php

--- profile.php
+++ profile.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+if (!isset($_SESSION['id'])) {
+    header('location: index.php');
+    exit();
+    echo $_SESSION['id'];
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,6 +18,10 @@
 </head>
 
 <body>
+    <form action="auth_logic/logout.php" method="POST">
+        <button class="button">Log Out</button>
+    </form>
+
     <H1>Hello World!</H1>
 </body>
 
